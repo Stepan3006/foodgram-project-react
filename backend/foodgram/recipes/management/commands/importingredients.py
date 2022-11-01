39bad855f9198d0634a9f59189f51de8fa419e0f
Stepan3006 <stepan@onepk.ru>
2022-11-01 20:11:58 +0300

    Fix some problems with dowload card

--- backend/foodgram/recipes/management/commands/importingredients.py
+++ backend/foodgram/recipes/management/commands/importingredients.py
@@ -1,57 +1,35 @@
-import csv
 import json
-import mimetypes
-import os
 
 from django.core.management.base import BaseCommand
 
-from recipes.models import Ingredient
+from recipes.models import Ingredient, Tag
 
 
 class Command(BaseCommand):
-    help = 'Импортирует ингридиенты'
-    filename: str
-    mimetype: str
 
     def add_arguments(self, parser):
-        parser.add_argument(
-            'filename', type=str, help='Путь к файлу .json/.csv с данными')
-        parser.add_argument(
-            '--keep-existing-data', action='store_true',
-            help='Предварительная очистка данных модели Ingredients')
-
-    def __load_data_from_file(self, filename, mimetype):
-        with open(filename, newline='') as fp:
-            try:
-                if mimetype == 'text/csv':
-                    data = csv.DictReader(
-                        fp, fieldnames=('name', 'measurement_unit'))
-                else:
-                    data = json.load(fp)
-            except (json.decoder.JSONDecodeError, TypeError):
-                self.stderr.write(f'Файл {filename} содержит ошибки')
-                raise SystemExit
-
-            for item in data:
-                yield item
+        parser.add_argument("--path", type=str, help="file path")
 
     def handle(self, *args, **options):
-        filename = options.get('filename')
-        wipe_data = not options.get('keep-exisiting-data')
+        file_path = options["path"]
 
-        if not os.path.isfile(filename):
-            self.stderr.write(f'Файл {filename} не найден')
-            raise SystemExit
-
-        mimetype = mimetypes.MimeTypes().guess_type(filename)[0]
-
-        if mimetype not in ('text/csv', 'application/json'):
-            self.stderr.write(f'Файл {filename} имеет запрещенный формат')
-            raise SystemExit
-
-        if wipe_data:
-            Ingredient.objects.all().delete()
-
-        for item in self.__load_data_from_file(filename, mimetype):
-            obj = Ingredient(**item)
-            obj.save()
+        with open(file_path, encoding='utf-8') as f:
+            jsondata = json.load(f)
+            if 'color' in jsondata[0]:
+                for line in jsondata:
+                    if not Tag.objects.filter(
+                       slug=line['slug']).exists():
+                        Tag.objects.create(
+                            name=line['name'],
+                            color=line['color'],
+                            slug=line['slug'],
+                        )
+            elif 'measurement_unit' in jsondata[0]:
+                for line in jsondata:
+                    if not Ingredient.objects.filter(
+                       name=line['name'],
+                       measurement_unit=line['measurement_unit']).exists():
+                        Ingredient.objects.create(
+                            name=line['name'],
+                            measurement_unit=line['measurement_unit']
+                        )
